8dd9323bb4e9d10a5f3b92fbc25db0206fbfbc85
Joon Kang <jooneywork@gmail.com>
2021-12-28 16:46:32 -0400

    Initial Commit

---
+++ graphics and stuff/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  graphics and stuff
+//
+//  Created by Joon Kang on 12/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ graphics and stuff/graphics_and_stuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  graphics_and_stuffApp.swift
+//  graphics and stuff
+//
+//  Created by Joon Kang on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct graphics_and_stuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
