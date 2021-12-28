028954cc41685e783e6e21a5ba18a58da165b13f
Joon Kang <jooneywork@gmail.com>
2021-12-28 17:20:29 -0400

    - add badge symbols

---
+++ graphics and stuff/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  graphics and stuff
+//
+//  Created by Joon Kang on 12/28/21.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
